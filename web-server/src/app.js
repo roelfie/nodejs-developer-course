e8072b1796a4c299851a3a91d85aee7b040bf29b
Roelfie <roelfie.kerstholt@gmail.com>
2020-01-20 16:12:48 +0100

    Basic handlebars setup (with npm package hbs) to serve dynamic content from Express.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,17 +1,50 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
+// Define paths for Express config
+const publicFolder = path.join(__dirname, '../public')
+const viewsFolder = path.join(__dirname, "../templates/views")
+const partialsFolder = path.join(__dirname, "../templates/partials")
 
 const app = express()
 
+// Setup Express view engine (handlebars) and templates directory (default is 'views').
+app.set('view engine', 'hbs') 
+app.set('views', viewsFolder)
+hbs.registerPartials(partialsFolder)
+
+// Setup static content directory
+app.use(express.static(publicFolder))
+
 app.get('', (req, res) => {
-    res.send('Hello express!')
+    // res.render('index') tells Express to automatically render templates/index.hbs
+    res.render('index', {
+        title: 'Weather',
+        name: 'Roelfie'
+    }) 
 })
 
 app.get('/about', (req, res) => {
-    res.send('About ...')
+    res.render('about', {
+        title: 'About Me',
+        name: 'Roelfie'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Roelfie'
+    })
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather ...')
+    res.send({
+        forecast: 'It is snowing',
+        location: 'Philadelphia'
+    })
 })
 
 app.listen(3000, () => {
