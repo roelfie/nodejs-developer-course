b0fde683739e334d7ca017f0479bb91edb9e4279
Roelfie <roelfie.kerstholt@gmail.com>
2020-01-25 15:37:05 +0100

    Use Express routers to register endpoints, and move them to seperate files.

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const Task = require('../models/task')
+const validateFields = require('../utils/validator')
 const router = new express.Router()
+
 
 router.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-
     try {
         await task.save()
         res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
+    } catch (err) {
+        res.status(400).send(err)
     }
 })
+
 
 router.get('/tasks', async (req, res) => {
     try {
@@ -22,56 +24,56 @@ router.get('/tasks', async (req, res) => {
     }
 })
 
+
 router.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-
     try {
-        const task = await Task.findById(_id)
-
+        const task = await Task.findById(req.params.id)
         if (!task) {
             return res.status(404).send()
         }
-
         res.send(task)
     } catch (e) {
-        res.status(500).send()
+        res.status(500).send(e)
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
+router.patch('/tasks/:id', async (req, res) => {
+    console.log('going validating')
+    if (!validateFields(req.body, Task.schema)) {
+        return res.status(400).send({error: 'One or more fields can not be updated.'})
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
+        const task = await Task.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { 
+                new: true, // return new task instead of original one.
+                runValidators: true
+            })
         if (!task) {
             return res.status(404).send()
         }
-
         res.send(task)
     } catch (e) {
+        // TODO distinguish between server errors (500) and validation errors (400)
         res.status(400).send(e)
     }
 })
 
+
 router.delete('/tasks/:id', async (req, res) => {
     try {
         const task = await Task.findByIdAndDelete(req.params.id)
-
         if (!task) {
-            res.status(404).send()
+            return res.status(404).send()
         }
-
         res.send(task)
     } catch (e) {
         res.status(500).send()
     }
 })
+
 
 module.exports = router
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,79 +1,78 @@
 const express = require('express')
 const User = require('../models/user')
+const validateFields = require('../utils/validator')
 const router = new express.Router()
 
-// router.post('/users', async (req, res) => {
-//     res.send('testing')
-/*    const user = new User(req.body)
 
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
     try {
         await user.save()
         res.status(201).send(user)
     } catch (e) {
-        res.status(400).send(e)
+        res.status(400).send(err)
     }
-*/
-// })
+})
+
 
 router.get('/users', async (req, res) => {
     try {
         const users = await User.find({})
         res.send(users)
-    } catch (e) {
+    } catch(e) {
         res.status(500).send()
     }
 })
+
 
 router.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
     try {
-        const user = await User.findById(_id)
-
+        const user = await User.findById(req.params.id)
         if (!user) {
             return res.status(404).send()
         }
-
         res.send(user)
     } catch (e) {
-        res.status(500).send()
+        res.status(500).send(e)
     }
 })
 
-router.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
 
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
+router.patch('/users/:id', async (req, res) => {
+    if (!validateFields(req.body, User.schema, ['name','email'])) {
+        return res.status(400).send({error: 'One or more fields can not be updated.'})
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
-
+        const user = await User.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            { 
+                new: true, // return new user instead of original one.
+                runValidators: true
+            })
         if (!user) {
             return res.status(404).send()
         }
-
         res.send(user)
     } catch (e) {
+        // TODO distinguish between server errors (500) and validation errors (400)
         res.status(400).send(e)
     }
 })
 
+
 router.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
-
         if (!user) {
             return res.status(404).send()
         }
-
         res.send(user)
     } catch (e) {
         res.status(500).send()
     }
 })
+
 
 module.exports = router
