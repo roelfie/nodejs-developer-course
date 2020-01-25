06acf675f7854111c8c91c1159508c5a924d8938
Roelfie <roelfie.kerstholt@gmail.com>
2020-01-25 15:18:03 +0100

    Added HTTP DELETE for users and tasks

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -68,6 +68,18 @@ app.patch('/users/:id', async (req, res) => {
     }
 })
 
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
     try {
@@ -120,6 +132,18 @@ app.patch('/tasks/:id', async (req, res) => {
     } catch (e) {
         // TODO distinguish between server errors (500) and validation errors (400)
         res.status(400).send(e)
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
